ab73c0503b0a0981c0ddc59cdb4c6626e98d4a69
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2022-04-28 02:06:21 +0600

    " сохранение .gitignore"

---
+++ Examples002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+// Console.Write("Введите ваше имя"); - будет писать текст в одной строке
+// Пользуемся git ignore для того чтоб скрыть лишние файлы в репозитории.
