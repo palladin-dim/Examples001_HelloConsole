ee30352fc6cbbbc6250d1ca31dada730f6a4214c
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2022-04-28 13:55:40 +0600

    "Сохранения конспект"

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -6,4 +6,13 @@ Console.WriteLine(numberA + numberB);
 // int numberA = 3;
 // int numberB = 4;
 // int result = numberA + numberB;
-
+// Пушит като по разному. То git push, то git push origin main, то 
+// git push --set-upstream origin main нихрена не понимаю.
+// после двух слэш. пишется коментарий. dotnet new console -создать новый проект. dotnet run - запустить компеляцию
+// Console.Write(); - будет писать текст в одной строке
+// Console.WriteLine(); - в конце прейти на новую строку
+// Console.ReadLine(); - считать строку  терминала
+// Пользуемся .gitignore для того чтоб скрыть лишние файлы в репозитории.
+// WriteLine  - то что будет выводится в терминале ("Hello, World!"). при сильных тормазах программы, нуно отключить автосохранения.
+// Через команду Console, обращаемся к окну терминала.
+// git commit - a вызывает редактор Subline Text/
