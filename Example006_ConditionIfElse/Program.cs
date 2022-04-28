8532eeccb265477f38a032e452e3d9cdb7cba077
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2022-04-28 19:36:10 +0600

    "modified:   Program.cs"

--- Example006_ConditionIfElse/Program.cs
+++ Example006_ConditionIfElse/Program.cs
@@ -9,7 +9,8 @@
 Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-// if(username == "Маша")
+// if(username = "Маша")
+
 if(username.ToLower == "Маша")
 {
     Console.WriteLine("Урра, это же Маша!");
