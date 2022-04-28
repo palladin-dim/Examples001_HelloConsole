72197fb3495a52d00a82c6da1e4a6ba0369a3cf8
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2022-04-28 13:41:16 +0600

    "сохранение через git add --all (почему то проблеммы с сохранением)"

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = 3;
+// int number -целое число
+int numberB = 4;
+Console.WriteLine(numberA + numberB);
+// можно вывемти так 
+// int numberA = 3;
+// int numberB = 4;
+// int result = numberA + numberB;
+
