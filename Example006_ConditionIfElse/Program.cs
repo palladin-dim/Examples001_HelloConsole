b728bc27e6bc071a7a77cb3ddb102e0b80fcfce9
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2022-04-28 19:10:38 +0600

    "commit .gitignore, Program.cs"

--- Example006_ConditionIfElse/Program.cs
+++ Example006_ConditionIfElse/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// if(Условие)
+// {
+//   Набор действий 1  
+// }
+// else(Иначе)
+// {
+//   Набор действий 2  
+// }
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+// if(username == "Маша")
+if(username.ToLower == "Маша")
+{
+    Console.WriteLine("Урра, это же Маша!");
+}
+else
+{
+    Console.Write("Приввет, ");
+    Console.WriteLine(username);
+}
+// Решение проблемы с регистром
+// После комады username в двенадатой строке нужно ввести ToLower 
+
+
