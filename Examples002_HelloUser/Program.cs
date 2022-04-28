86900491244b383f9ad47145e75fe6e33fa698ab
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2022-04-28 14:48:00 +0600

    "продолжение"

--- Examples002_HelloUser/Program.cs
+++ Examples002_HelloUser/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Введите ваше имя");
-String username = Console.WriteLine();
+string username = Console.ReadLine();
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
 // Console.Write(); - будет писать текст в одной строке
 // Console.WriteLine(); - в конце прейти на новую строку
-// Console.ReadLine(); - считать строку  терминала
+// Console.ReadLine(); - считать строку  
 // Пользуемся .gitignore для того чтоб скрыть лишние файлы в репозитории.
